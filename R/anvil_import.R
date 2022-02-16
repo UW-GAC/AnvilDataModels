7c75e5e0b57d3ca3b8d7f6d2fdad3a675fe9c99c
Stephanie M. Gogarten <sdmorris@uw.edu>
2022-02-16 00:24:10 +0000

    check existing table in import function

--- R/anvil_import.R
+++ R/anvil_import.R
@@ -9,12 +9,41 @@
 #' 
 #' @import AnVIL
 #' @export
-anvil_import_table <- function(table, table_name, model) {
-    # does table already exist?
-    # - do the columns match?
-    # - are there any overlapping samples?
-    
+anvil_import_table <- function(table, table_name, model, overwrite=FALSE) {
+    # add entity id first, so we can compare to anvil
     table <- add_entity_id(table, table_name, model)
+  
+    # does table already exist?
+    anvil_tables <- avtables()
+    if (table_name %in% anvil_tables$table) {
+        message("Table ", table_name, " already exists")
+        anvil_table <- avtable(table_name)
+        
+        # do the columns match?
+        if (!setequal(names(table), names(anvil_table))) {
+            stop("Columns of new table do not match existing table\n",
+                 "  AnVIL: ", paste(names(anvil_table), collapse=", "), "\n",
+                 "  table: ", paste(names(table), collapse=", "))
+        }
+        
+        # are there any overlapping entities?
+        # from AnVIL docs: Terra will only overwrite data rows with the same ID 
+        # (in the first column). If the TSV (load) file includes different IDs, 
+        # these rows will be added to the existing table.
+        entity_id <- paste0(table_name, "_id")
+        anvil_entity <- anvil_table[[entity_id]]
+        this_entity <- table[[entity_id]]
+        overlaps <- length(intersect(this_entity, anvil_entity))
+        if (overlaps > 0) {
+            if (overwrite) {
+              message("  Overwriting ", overlaps, " rows")
+            } else {
+                stop("Some entities in table '", table_name, "' already exist\n",
+                     "  Set overwrite=TRUE to overwrite these rows")
+            }
+        }
+    }
+    
     avtable_import(table, entity=paste0(table_name, "_id"))
 }
 
