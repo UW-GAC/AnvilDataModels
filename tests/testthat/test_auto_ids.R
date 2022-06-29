bc4c3df22c99c3f407feb82f8bbfa35942d3f933
Stephanie M. Gogarten <sdmorris@uw.edu>
2022-06-29 12:50:42 -0700

    unit tests for auto ids

---
+++ tests/testthat/test_auto_ids.R
@@ -0,0 +1,20 @@
+
+test_that("auto ids", {
+    tsv <- system.file("extdata", "data_model_auto_id.tsv", package="AnvilDataModels")
+    model <- tsv_to_dm(tsv)
+    table_name <- "analysis"
+    file <- system.file("extdata", "analysis1.tsv", package="AnvilDataModels")
+    tables <- read_data_tables(file, table_names=table_name, quiet=TRUE)
+    tables2 <- add_auto_columns(tables[[table_name]], table_name=table_name, model)
+    expect_true("analysis_id" %in% names(tables2))
+})
+
+test_that("table with no auto ids", {
+    tsv <- system.file("extdata", "data_model_auto_id.tsv", package="AnvilDataModels")
+    model <- tsv_to_dm(tsv)
+    table_name <- "file"
+    file <- system.file("extdata", "analysis_file1.tsv", package="AnvilDataModels")
+    tables <- read_data_tables(file, table_names=table_name, quiet=TRUE)
+    tables2 <- add_auto_columns(tables[[table_name]], table_name=table_name, model)
+    expect_equal(tables$file, tables2)
+})
