0da0bed56a3731334ca9a4c6bc996e66a8256506
Stephanie M. Gogarten <sdmorris@uw.edu>
2023-05-17 14:47:19 -0700

    add print statement for debugging

--- R/anvil_import.R
+++ R/anvil_import.R
@@ -194,6 +194,7 @@ add_entity_id <- function(table, table_name, model) {
         }
         Sys.sleep(60)
         for (t in names(job_status)) {
+            print(job_status[[t]])
             job_status[[t]] <- avtable_import_status(job_status[[t]])
         }
         js <- bind_rows(job_status)$status
